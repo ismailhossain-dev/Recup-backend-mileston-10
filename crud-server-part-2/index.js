b6db30a4be1d911de503a1b14285feacbbe65cea
sabbir <programmarsabbir@gmail.com>
2026-02-05 16:50:10 +0600

    complete part2

--- crud-server-part-2/index.js
+++ crud-server-part-2/index.js
@@ -1,7 +1,7 @@
 //express setup
 const express = require("express");
 //mongodb
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 //connection server to client cors req structure
 const cors = require("cors");
 const app = express();
@@ -9,6 +9,7 @@ const app = express();
 const port = process.env.PORT || 3000;
 //middleware connection backend & server
 app.use(cors());
+//client teke server data receive korbe express website
 app.use(express.json());
 //mongodb connection or location
 const uri =
@@ -23,6 +24,7 @@ const client = new MongoClient(uri, {
 });
 
 //finished mongodb code
+//this express code root
 app.get("/", (req, res) => {
   res.send("Hello Crud operation");
 });
@@ -31,6 +33,81 @@ async function run() {
   try {
     //uporer client er sathe try add kore diyesi
     await client.connect();
+    const userDB = client.db("userDB");
+    const userCollection = userDB.collection("users");
+    const practiceVideo4 = userDB.collection("practice4");
+    //add database related apis here
+    app.post("/users", async (req, res) => {
+      //client ja ja kichu pataichi seta req.body pabo
+      const newUser = req.body;
+      console.log(newUser);
+      //send data database or mongodb
+      const result = await userCollection.insertOne(newUser);
+      res.send(result);
+    });
+
+    //v:4 practice post
+    app.post("/practice4", async (req, res) => {
+      //receive clint data
+      const user = req.body;
+      console.log("practice route hitting");
+      const result = await practiceVideo4.insertOne(user);
+      res.send(result);
+    });
+
+    //v:5 get data
+    app.get("/user", async (req, res) => {
+      const cursor = userCollection.find();
+      //cursor take array te convert korar jorno await use korbo
+      const result = await cursor.toArray();
+      res.send(result);
+    });
+
+    //v:6
+    //delete user
+    app.delete("/users/:id", async (req, res) => {
+      //req.params er mardome amra client teke information nei
+      // console.log(req.params.id);
+      const id = req.params.id;
+      //database id take object hisabe tai amra client id take object convert kortesi
+      const query = { _id: new ObjectId(id) };
+      const result = await userCollection.deleteOne(query);
+      res.send(result);
+    });
+
+    //getting single data for user
+    //test website http://localhost:3000/user/3045034549455
+    app.get("/user/:id", async (req, res) => {
+      const id = req.params.id;
+      //client id ke objectid te convert korthe and find kortesi
+      const query = { _id: new ObjectId(id) };
+      console.log("need with id", id);
+      const result = await userCollection.findOne(query);
+      res.send(result);
+    });
+
+    //update data api
+    app.patch("/users/:id", async (req, res) => {
+      //client teke frontend value ta niye asteses
+      const id = req.params.id;
+      //UpdateUser data receive
+      const updateUser = req.body;
+      console.log("to update", id, updateUser);
+
+      //send id mongodb and covert the object
+      const query = { _id: new ObjectId(id) };
+      const update = {
+        $set: {
+          name: updateUser.name,
+          email: updateUser.email,
+        },
+      };
+      //optional
+      const options = {};
+      const result = await userCollection.updateOne(query, update);
+      res.send(result);
+    });
+
     //ping er mardome terminal bujtehe pari mongodb off naki on
     await client.db("admin").command({ ping: 1 });
     //connect hole console e dekabe
